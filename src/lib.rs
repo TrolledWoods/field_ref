96ee98d086724260f55fd4d0dcda7b7ec0a22650
John Hörnvall <trolledwoods@gmail.com>
2021-03-29 17:46:49 +0200

    Basic group stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-use std::cmp::{Ord, Ordering, PartialOrd};
-use std::fmt;
-use std::hash::{Hash, Hasher};
-use std::marker::PhantomData;
+#![no_std]
+
+use core::cmp::{Ord, Ordering, PartialOrd};
+use core::fmt;
+use core::hash::{Hash, Hasher};
+use core::marker::PhantomData;
+use core::mem::MaybeUninit;
 
 /// This creates a [`FieldRef`] to a field of a type. The type cannot implement [`Deref`](core::ops::Deref), because deref can run arbitrary
 /// code, which would mean that the method this library uses to reference fields isn't guaranteed to be correct.
@@ -30,15 +33,135 @@ macro_rules! field_ref {
             trait AmbiguousIfImpl<A> { fn some_item() { } }
 
             impl<T: ?Sized> AmbiguousIfImpl<()> for Check<T> { }
-            impl<T: ?Sized + std::ops::Deref> AmbiguousIfImpl<u8> for Check<T> { }
+            impl<T: ?Sized + core::ops::Deref> AmbiguousIfImpl<u8> for Check<T> { }
 
             <Check::<$on> as AmbiguousIfImpl<_>>::some_item() // Your type probably implements 'Defer'. It's not allowed to
         };
 
         // Because the type does not implement Deref this is safe to do!
-        let temp = std::mem::MaybeUninit::<$on>::uninit();
-        unsafe { $crate::FieldRef::from_pointers(temp.as_ptr(), std::ptr::addr_of!((*temp.as_ptr()).$field)) }
+        let temp = core::mem::MaybeUninit::<$on>::uninit();
+        unsafe { $crate::FieldRef::from_pointers(temp.as_ptr(), core::ptr::addr_of!((*temp.as_ptr()).$field)) }
     }};
+}
+
+/// A group of non-overlapping fields. Created with the [`group`] function.
+pub struct FieldGroup<On, Field, const N: usize> {
+    // Invariants:
+    // * The fields cannot overlap
+    fields: [FieldRef<On, Field>; N],
+}
+
+impl<On, Field, const N: usize> FieldGroup<On, Field, N> {
+    /// Iterates over all the fields in order.
+    pub fn iter<'a>(&'a self, on: &'a On) -> impl Iterator<Item = &'a Field> {
+        let on_ptr = on as *const On;
+        self.fields.iter().enumerate().map(move |(i, field)| {
+            // Safety: The invariants of the field ensure this is safe, as well as the invariant
+            // that the fields cannot overlap.
+            unsafe { &*on_ptr.cast::<u8>().add(field.offset).cast::<Field>() }
+        })
+    }
+
+    /// Iterates over all the fields in order.
+    pub fn iter_mut<'a>(&'a self, on: &'a mut On) -> impl Iterator<Item = &'a mut Field> {
+        let on_ptr = on as *mut On;
+        self.fields.iter().enumerate().map(move |(i, field)| {
+            // Safety: The invariants of the field ensure this is safe, as well as the invariant
+            // that the fields cannot overlap.
+            unsafe { &mut *on_ptr.cast::<u8>().add(field.offset).cast::<Field>() }
+        })
+    }
+
+    /// Reads all the fields in the group from a value. The returned fields are in the same order
+    /// as given to the [`group`] function.
+    pub fn get<'a>(&self, on: &'a On) -> [&'a Field; N] {
+        let mut temp = MaybeUninit::<[&Field; N]>::uninit();
+
+        let on_ptr = on as *const On;
+        let field_ptr = temp.as_mut_ptr().cast::<&Field>();
+        for (i, field) in self.fields.iter().enumerate() {
+            // Safety: The invariants of the field ensure this is safe
+            unsafe {
+                field_ptr
+                    .add(i)
+                    .write(&*on_ptr.cast::<u8>().add(field.offset).cast::<Field>());
+            }
+        }
+
+        // Safety: We wrote to all the indices in the array in the loop
+        unsafe { temp.assume_init() }
+    }
+
+    /// Reads all the fields in the group from a value. The returned fields are in the same order
+    /// as given to the [`group`] function.
+    pub fn get_mut<'a>(&self, on: &'a mut On) -> [&'a mut Field; N] {
+        let mut temp = MaybeUninit::<[&mut Field; N]>::uninit();
+
+        let on_ptr = on as *mut On;
+        let field_ptr = temp.as_mut_ptr().cast::<&mut Field>();
+        for (i, field) in self.fields.iter().enumerate() {
+            // Safety: The invariants of the field ensure this is safe, as well as the invariant
+            // that the fields cannot overlap.
+            unsafe {
+                field_ptr
+                    .add(i)
+                    .write(&mut *on_ptr.cast::<u8>().add(field.offset).cast::<Field>());
+            }
+        }
+
+        // Safety: We wrote to all the indices in the array in the loop
+        unsafe { temp.assume_init() }
+    }
+}
+
+/// Tries to create a [`FieldGroup`] with all the given fields. Returns [`None`] if there are overlapping
+/// fields.
+///
+/// # Examples
+/// ```
+/// use field_ref::{field_ref, group};
+///
+/// struct Testing {
+///     a: u32,
+///     b: u32,
+///     c: u32,
+/// }
+///
+/// let mut test = Testing {
+///     a: 0,
+///     b: 0,
+///     c: 0,
+/// };
+///
+/// let fields = group([
+///     field_ref!(Testing, a),
+///     field_ref!(Testing, b),
+///     field_ref!(Testing, c),
+/// ]).unwrap();
+///
+/// let [a, b, c] = fields.get_mut(&mut test);
+/// *a = 42;
+/// *b = 30;
+/// *c = 10;
+///
+/// assert_eq!(test.a, 42);
+/// assert_eq!(test.b, 30);
+/// assert_eq!(test.c, 10);
+/// ```
+pub fn group<On, Field, const N: usize>(
+    fields: [FieldRef<On, Field>; N],
+) -> Option<FieldGroup<On, Field, N>> {
+    // Check for overlap
+    for i in 0..fields.len() {
+        for j in i + 1..fields.len() {
+            if fields[i] == fields[j] {
+                return None;
+            }
+        }
+    }
+
+    // Since there was no overlap, the invariant is fulfilled
+    Some(FieldGroup { fields })
 }
 
 /// A very lightweight reference to a struct field.
@@ -46,8 +169,12 @@ macro_rules! field_ref {
 /// It's guaranteed to be the exact same representation as a [`usize`]
 #[repr(C)]
 pub struct FieldRef<On, Field> {
-    // Invariant: This has to be a valid offset from the start of an 'On' struct that produces the
+    // Invariants:
+    // * This has to be a valid offset from the start of an 'On' struct that produces the
     // field. It's in bytes.
+    // * Fields cannot overlap. This means we can't do tricks like [T; 3] having fields with type [T; 2], even though that's
+    // theoretically possible, as I think it's more useful to be able to partially borrow the
+    // types.
     offset: usize,
     _phantom: PhantomData<(On, Field)>,
 }
@@ -63,7 +190,7 @@ impl<On, Field> FieldRef<On, Field> {
     /// * The offset between field and on has to be valid to pass to `from_offset`
     pub unsafe fn from_pointers(on: *const On, field: *const Field) -> Self {
         assert_eq!(
-            (field as usize - on as usize) % std::mem::align_of::<Field>(),
+            (field as usize - on as usize) % core::mem::align_of::<Field>(),
             0,
             "Cannot create a 'FieldRef' to an unaligned field"
         );
@@ -129,7 +256,7 @@ impl<On, Field> FieldRef<On, Field> {
 
     /// Replaces the value of a field, and returns the old value
     pub fn replace(self, on: &mut On, new: Field) -> Field {
-        std::mem::replace(self.get_mut(on), new)
+        core::mem::replace(self.get_mut(on), new)
     }
 
     /// Joins fields, i.e. takes a field of a field.
@@ -150,6 +277,14 @@ impl<On, Field> FieldRef<On, Field> {
         // nexts offset creates a valid 'T' from a 'Field'. So, we should always get a valid
         // 'T' from an 'On' by putting them together.
         unsafe { FieldRef::from_offset(self.offset + next.offset) }
+    }
+}
+
+/// The field that this returns will just give back the same instance. Might be useful for generic
+/// code.
+impl<T> Default for FieldRef<T, T> {
+    fn default() -> Self {
+        unsafe { Self::from_offset(0) }
     }
 }
 
